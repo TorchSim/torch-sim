7e553ec97ae04415485c524b9975012c2e1eca25
abhijeetgangan <abhijeet.gangan261997@gmail.com>
2025-03-07 12:35:00 -0800

    Final try at Fairchem example

--- examples/1_Introduction/1.5_Fairchem.py
+++ examples/1_Introduction/1.5_Fairchem.py
@@ -4,10 +4,10 @@
 # /// script
 # dependencies = [
 #     "fairchem-core>=1.6",
-#     "torch>=2.5.0",
+#     "torch>=2.4.0,<2.5.0",
 # ]
 # extra_install = [
-#     "pip install torch_geometric pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.5.0+cpu.html"
+#     "pip install torch_geometric pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.4.0+cpu.html"
 # ]
 # ///
 
