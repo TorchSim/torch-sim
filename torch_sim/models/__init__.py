0136ef2b925fb613d4e2808b179117aa5ab98a74
orionarcher <ocohen@radical-ai.com>
2025-03-16 02:30:54 +0000

    pull model import up a level

--- torch_sim/models/__init__.py
+++ torch_sim/models/__init__.py
@@ -1,1 +1,15 @@
 """Models for Torch-Sim."""
+
+from torch_sim.models.mace import MaceModel
+from torch_sim.models.soft_sphere import SoftSphereModel
+from torch_sim.models.fairchem import FairChemModel
+from torch_sim.models.lennard_jones import LennardJonesModel
+from torch_sim.models.morse import MorseModel
+
+__all__ = [
+    "MaceModel",
+    "SoftSphereModel",
+    "FairChemModel",
+    "LennardJonesModel",
+    "MorseModel",
+]
