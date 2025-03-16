236f1956d176843e5b139073932c6373794e72df
orionarcher <ocohen@radical-ai.com>
2025-03-16 02:31:05 +0000

    lint

--- torch_sim/models/__init__.py
+++ torch_sim/models/__init__.py
@@ -1,15 +1,16 @@
 """Models for Torch-Sim."""
 
-from torch_sim.models.mace import MaceModel
-from torch_sim.models.soft_sphere import SoftSphereModel
 from torch_sim.models.fairchem import FairChemModel
 from torch_sim.models.lennard_jones import LennardJonesModel
+from torch_sim.models.mace import MaceModel
 from torch_sim.models.morse import MorseModel
+from torch_sim.models.soft_sphere import SoftSphereModel
+
 
 __all__ = [
-    "MaceModel",
-    "SoftSphereModel",
     "FairChemModel",
     "LennardJonesModel",
+    "MaceModel",
     "MorseModel",
+    "SoftSphereModel",
 ]
