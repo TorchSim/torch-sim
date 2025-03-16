3528f59ba10818730f15cf5f2799da0262c9ff1e
orionarcher <ocohen@radical-ai.com>
2025-03-16 02:34:22 +0000

    fix imports in model init

--- torch_sim/models/__init__.py
+++ torch_sim/models/__init__.py
@@ -1,16 +1,33 @@
 """Models for Torch-Sim."""
 
-from torch_sim.models.fairchem import FairChemModel
 from torch_sim.models.lennard_jones import LennardJonesModel
-from torch_sim.models.mace import MaceModel
 from torch_sim.models.morse import MorseModel
 from torch_sim.models.soft_sphere import SoftSphereModel
 
 
+try:
+    from torch_sim.models.fairchem import FairChemModel
+
+    _has_fairchem = True
+except ImportError:
+    _has_fairchem = False
+
+try:
+    from torch_sim.models.mace import MaceModel
+
+    _has_mace = True
+except ImportError:
+    _has_mace = False
+
+
 __all__ = [
-    "FairChemModel",
     "LennardJonesModel",
-    "MaceModel",
     "MorseModel",
     "SoftSphereModel",
 ]
+
+if _has_fairchem:
+    __all__ += ["FairChemModel"]
+
+if _has_mace:
+    __all__ += ["MaceModel"]
