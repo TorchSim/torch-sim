96ec51fc3a0b7f1bd0c637a4b60f90867f67edae
orionarcher <ocohen@radical-ai.com>
2025-03-20 18:13:18 +0000

    small fix to fairchem stressed for unbatched systems

--- torch_sim/models/fairchem.py
+++ torch_sim/models/fairchem.py
@@ -291,4 +291,6 @@ class FairChemModel(torch.nn.Module, ModelInterface):
             results[key] = _pred.detach()
 
         results["energy"] = results["energy"].squeeze(dim=1)
+        if len(results["stress"].shape) == 2:
+            results["stress"] = results["stress"].unsqueeze(dim=0)
         return results
