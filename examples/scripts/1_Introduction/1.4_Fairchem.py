141230ace62636519bc22508bc81487a871fda11
abhijeetgangan <abhijeet.gangan261997@gmail.com>
2025-03-30 13:42:00 -0700

    Update fairchem script

--- examples/scripts/1_Introduction/1.4_Fairchem.py
+++ examples/scripts/1_Introduction/1.4_Fairchem.py
@@ -6,8 +6,8 @@
 #     "fairchem-core>=1.6",
 #     "torch>=2.4.0,<2.5.0",
 # ]
-# extra_install = [
-#     "pip install -r fairchem-repo/packages/requirements.txt"
+# install_commands = [
+#     "pip install torch_geometric pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.4.0+cpu.html"
 # ]
 # ///
 
