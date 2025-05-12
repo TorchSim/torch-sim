463f9c7571980e6934dff76770a24b8cf0de93bc
Rhys Goodall <rhys.goodall@outlook.com>
2025-05-12 10:32:47 -0400

    fea: test stress

--- tests/models/conftest.py
+++ tests/models/conftest.py
@@ -5,6 +5,7 @@ import pytest
 import torch
 
 import torch_sim as ts
+from torch_sim.elastic import full_3x3_to_voigt_6_stress
 
 
 if typing.TYPE_CHECKING:
@@ -93,6 +94,20 @@ def make_model_calculator_consistency_test(
             rtol=rtol,
             atol=atol,
         )
+
+        if "stress" in model_results:
+            calc_stress = torch.tensor(
+                atoms.get_stress(),
+                device=device,
+                dtype=model_results["stress"].dtype,
+            ).unsqueeze(0)
+
+            torch.testing.assert_close(
+                full_3x3_to_voigt_6_stress(model_results["stress"]),
+                calc_stress,
+                rtol=rtol,
+                atol=atol,
+            )
 
     # Rename the function to include the test name
     test_model_calculator_consistency.__name__ = f"test_{test_name}_consistency"
