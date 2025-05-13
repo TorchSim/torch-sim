fix: sevennet sign convention for stress wrong

torch_sim/models/sevennet.py
@@ -250,6 +250,6 @@ class SevenNetModel(torch.nn.Module, ModelInterface):
 
         stress = output[key.PRED_STRESS]
         if stress is not None:
-            results["stress"] = voigt_6_to_full_3x3_stress(stress.detach())
+            results["stress"] = -voigt_6_to_full_3x3_stress(stress.detach())
 
         return results