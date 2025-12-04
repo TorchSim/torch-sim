Fix: Units Issue#309

examples/scripts/3_Dynamics/3.1_Lennard_Jones_NVE.py
@@ -98,7 +98,7 @@ results = model(state)
 
 # Set up NVE simulation
 # kT: initial temperature in metal units (K)
-# dt: timestep in metal units (ps)
+# dt: timestep in metal units (fs)
 kT = torch.tensor(80 * Units.temperature, device=device, dtype=dtype)
 dt = torch.tensor(0.001 * Units.time, device=device, dtype=dtype)
 

examples/scripts/3_Dynamics/3.9_MACE_NVT_staggered_stress.py
@@ -49,7 +49,7 @@ model = MaceModel(
 
 state = ts.io.atoms_to_state(si_dc, device=device, dtype=dtype)
 
-dt = 0.002 * Units.time  # Timestep (ps)
+dt = 0.002 * Units.time  # Timestep (fs)
 kT = (
     torch.tensor(1000, device=device, dtype=dtype) * Units.temperature
 )  # Initial temperature (K)