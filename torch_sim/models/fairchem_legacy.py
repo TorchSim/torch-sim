8bb856bb89417cc89d426acd492716ecc2a82ea1
Rhys Goodall <rhys.goodall@outlook.com>
2026-02-07 12:45:01 -0500

    Fairchem v1 should have a planned deprecation date

--- torch_sim/models/fairchem_legacy.py
+++ torch_sim/models/fairchem_legacy.py
@@ -416,6 +416,9 @@ class FairChemV1Model(ModelInterface):
         for i, (n, c) in enumerate(
             zip(natoms, torch.cumsum(natoms, dim=0), strict=False)
         ):
+            # NOTE: Legacy FairChem models (v1) do not support charge/spin,
+            # so we don't pass these fields to the Data object.
+            # The model will simply ignore charge/spin and treat all systems as neutral.
             data_list.append(
                 Data(
                     pos=state.positions[c - n : c].clone(),
@@ -424,8 +427,6 @@ class FairChemV1Model(ModelInterface):
                     fixed=fixed[c - n : c].clone(),
                     natoms=n,
                     pbc=state.pbc,
-                    charge=state.charge[i].clone(),
-                    spin=state.spin[i].clone(),
                 )
             )
         self.data_object = Batch.from_data_list(data_list)
