02aceff66807d4e2fea17af39726d08132f7b023
Reilly Osadchey Brown <rosadcheybrown@gmail.com>
2026-03-04 16:58:46 -0500

    Fix lint: update type suppression and remove broken codespell comment

--- torch_sim/models/graphpes_framework.py
+++ torch_sim/models/graphpes_framework.py
@@ -185,5 +185,5 @@ class GraphPESWrapper(ModelInterface):
         if not isinstance(cutoff, torch.Tensor):
             raise TypeError("GraphPES model cutoff must be a tensor")
         atomic_graph = state_to_atomic_graph(state, cutoff)
-        preds = self._gp_model.predict(atomic_graph, self._properties)
-        return {k: v.detach() for k, v in preds.items()}  # type: ignore[return-value]
+        preds = self._gp_model.predict(atomic_graph, self._properties)  # ty: ignore[call-non-callable]
+        return {k: v.detach() for k, v in preds.items()}
